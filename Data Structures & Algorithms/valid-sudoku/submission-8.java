180772a2b950220c85bff4ec628515cab53d862e
Matheus Marcelino <csfyre2403@gmail.com>
2026-09-14 14:47:20 -0600

    Add: valid-sudoku - submission-8

---
+++ Data Structures & Algorithms/valid-sudoku/submission-8.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<Character>[] rows = new HashSet[9];
+        Set<Character>[] cols = new HashSet[9];
+        Set<Character>[] squares = new HashSet[9];
+
+        for (int i = 0; i < 9; i++) {
+            rows[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+            squares[i] = new HashSet<>();
+        }
+
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                char sqr = board[r][c];
+                if (sqr == '.') continue;
+
+                int sqrIdx = (r / 3) * 3 + (c / 3);
+
+                if (!rows[r].add(sqr) || !cols[c].add(sqr) || !squares[sqrIdx].add(sqr)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
