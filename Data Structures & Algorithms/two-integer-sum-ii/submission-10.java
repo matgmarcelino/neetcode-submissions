cc64857bb8744493038a48a11e9ae2373fabccba
Matheus Marcelino <csfyre2403@gmail.com>
2026-09-15 09:19:47 -0600

    Add: two-integer-sum-ii - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-10.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum > target) {
+                right--;
+                continue;
+            }
+
+            if (sum < target) {
+                left++;
+                continue;
+            }
+
+            return new int[] { left + 1, right + 1 };
+        }
+
+        return new int[2];
+    }
+}
