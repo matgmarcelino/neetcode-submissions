0d599177592782e5c29f26393717dc150d2bb5ae
Matheus Marcelino <csfyre2403@gmail.com>
2026-09-16 11:59:00 -0600

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+        
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+        
+        for (int i = 0; i < s1.length(); i++) {
+            count1[s1.charAt(i) - 'a']++;
+            count2[s2.charAt(i) - 'a']++;
+        }
+
+        int matches = 0;
+        for (int i = 0; i < 26; i++) {
+            if (count1[i] == count2[i]) {
+                matches++;
+            }
+        }
+
+        int l = 0;
+        for (int r = s1.length(); r < s2.length(); r++) {
+            if (matches == 26) return true;
+
+            int idx = s2.charAt(r) - 'a';
+            count2[idx]++;
+            
+            if (count1[idx] == count2[idx]) {
+                matches++;
+            } else if (count1[idx] + 1 == count2[idx]) {
+                matches--;
+            }
+
+            idx = s2.charAt(l) - 'a';
+            count1[idx]++;
+
+            if (count1[idx] == count2[idx]) {
+                matches++;
+            } else if (count1[idx] == count2[idx] + 1) {
+                matches--;
+            }
+            l++;
+        }
+
+        return matches == 26;
+    }
+}
